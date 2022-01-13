91678208e69e206b2afe4cc05349f57d6eea8d6c
Rexter Dayuta <rexterdayuta@gmail.com>
2022-01-14 00:31:58 +0800

    Delete config.php

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-const ow_base_url = 'https://yourconference.live/Kickoff2022/';
-
-const ow_db_hostname = 'localhost';
-const ow_db_username = 'kickoff2022';
-const ow_db_password = 'Kw55nm_2';
-const ow_db_database = 'kickoff2022';
