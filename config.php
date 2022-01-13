5974c0be20c15c1a9f64c4ffc4ad9717aa2977a3
Rexter Dayuta <rexterdayuta@gmail.com>
2022-01-14 00:18:42 +0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+const ow_base_url = 'https://yourconference.live/Kickoff2022/';
+
+const ow_db_hostname = 'localhost';
+const ow_db_username = 'kickoff2022';
+const ow_db_password = 'kickoff2022#';
+const ow_db_database = 'kickoff2022';
