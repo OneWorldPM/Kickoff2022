e35a176b2ba435198592acea88e5d340212feaf1
Athul AK <athullive@gmail.com>
2022-01-13 16:53:15 +0100

    Config file sample

---
+++ config.sample.php
@@ -0,0 +1,7 @@
+<?php
+const ow_base_url = 'https://yourconference.live/Kickoff2022/';
+
+const ow_db_hostname = 'localhost';
+const ow_db_username = 'Kickoff2022';
+const ow_db_password = 'kickoff2022#';
+const ow_db_database = 'Kickoff2022';
