941cfb2e03126bc360455962392fdf18bc5ff6c9
Rexter Dayuta <rexterdayuta@gmail.com>
2022-01-18 20:53:12 +0800

    modal updates

--- application/views/home-new-modal.php
+++ application/views/home-new-modal.php
@@ -9,18 +9,18 @@
                 </button>
             </div>
             <div class="modal-body">
-                REGISTRATION <br>
+                <b>REGISTRATION </b><br>
                 Registration will be open from 9:00 AM to 5:00 PM on Tuesday, February 22. Upon arrival at the Hilton, check-in at the front desk then head to the 2022 Commercial Kickoff registration desk on the second floor of the hotel to pick up your name badge. The Welcome Reception begins at 6:00 PM
                 <br><br>
                 Though a complete day-by-day agenda will be provided at the meeting registration desk, here’s an overview of the week (subject to change):
                 <br><br>
-                Tuesday, February 22<br>
+                <b style="color: #fd7e14">Tuesday, February 22</b><br>
                 9:00 AM – 12:00 PM<br>
                 12:00 PM – 5:00 PM	Leadership Registration<br>
                 Leadership Lunch/Meeting<br>
                 2:00 PM – 5:00 PM	General Attendee Registration<br>
                 6:00 PM – 8:30 PM	Welcome Reception and Product/Solution Showcase<br><br>
-                Wednesday, February 23<br>
+                <b style="color: #fd7e14">Wednesday, February 23</b><br>
                 7:00 AM – 8:00 AM	Breakfast<br>
                 8:00 AM – 10:00 AM	Opening General Session<br>
                 10:00 AM–12:00 PM	Training Sessions<br>
@@ -29,7 +29,7 @@
                 3:00 PM – 5:30 PM	Product/Solution Showcase: Interactive Solution Experience<br>
                 5:30 PM – 6:00 PM	Free Time<br>
                 6:00 PM – 9:00 PM	All Team Offsite Dinner<br><br>
-                Thursday, February 24<br>
+                <b style="color: #fd7e14">Thursday, February 24</b><br>
                 7:00 AM – 8:00 AM	Breakfast<br>
                 8:00 AM – 10:00 AM	Training Session Day Two Kickoff<br>
                 10:00 AM–12:00 PM	Training Sessions<br>
@@ -39,7 +39,7 @@
                 4:30 PM – 5:45 PM	Closing General Session<br>
                 5:45 PM – 7:00 PM	Free Time<br>
                 7:00 PM – 12:00 AM	Awards Dinner, Ceremony and Celebration<br><br>
-                Friday, February 25<br>
+                <b style="color: #fd7e14">Friday, February 25</b><br>
                 All morning	Departures<br>
 
             </div>
@@ -61,31 +61,31 @@
                 </button>
             </div>
             <div class="modal-body">
-                This year’s hotel accommodations are based at the Hilton Anaheim. Training sessions and several social events will take place inside the hotel.
+                This year’s hotel accommodations are based at the <a href="https://www3.hilton.com/en/hotels/california/hilton-anaheim-SNAAHHH/index.html?SEO_id=GMB-HI-SNAAHHH&y_source=1_MTIyMDk1OS03MTUtbG9jYXRpb24uZ29vZ2xlX3dlYnNpdGVfb3ZlcnJpZGU%3D">Hilton Anaheim</a>. Training sessions and several social events will take place inside the hotel.
                 <br><br>
                 Hotel reservations will be centrally managed by Todd Gabello, and a room will be held in your name once you complete your registration. Your confirmation number will be emailed to you no later than the week of February 14.
                 <br><br>
-                ARRIVALS<br>
-                All attendees will arrive on Tuesday, February 22. Please refer to the January 17 email from kickoff@agilitihealth.com for details on what time you need to be at the hotel by on Tuesday.
+                <b>ARRIVALS</b><br>
+                All attendees will arrive on Tuesday, February 22. Please refer to the January 17 email from <a href="https://kickoff@agilitihealth.com">kickoff@agilitihealth.com</a> for details on what time you need to be at the hotel by on Tuesday.
                 <br><br>
-                DEPARTURES<br>
+                <b>DEPARTURES</b><br>
                 All attendees will depart on Friday, February 25. You may book your departure flight as early as you would like that morning from John Wayne Airport (SNA).
                 <br><br>
-                AIR TRAVEL<br>
+                <b>AIR TRAVEL</b><br>
                 Those flying to the meeting should book a flight in and out of John Wayne Airport (SNA).
                 <br><br>
                 All attendees must book flights using the link that was embedded in the registration site. This will ensure that reservations are booked under the Agiliti account and will appear on our arrival/departure reports. If you book outside the registration site, your flight cost will not be covered by the 2022 Commercial Kickoff budget, and you will need to expense the flight through your individual cost center.
                 <br><br>
-                If you have any questions regarding your travel schedule, please contact Todd Gabello.
+                If you have any questions regarding your travel schedule, please contact <a href="mailto:Todd.Gabello@agilitihealth.com">Todd Gabello</a> .
                 <br><br>
-                GETTING AROUND TOWN<br>
+                <b>GETTING AROUND TOWN</b><br>
                 Transportation to the Hilton Anaheim will be available upon arrival at John Wayne Airport (SNA). Greeters stationed in the baggage claim area with Agiliti signs will direct you to the nearest shuttle.
                 <br><br>
                 Departure transportation will be available on Friday, February 25 from the Hilton Anaheim to John Wayne Airport (SNA).
                 <br><br>
                 Rental cars will not be reimbursed. If your arrival/departure date/time falls outside the shuttle schedule, please take an Uber, Lyft or Taxi.
                 <br><br>
-                LOCAL PARKING<br>
+                <b>LOCAL PARKING</b><br>
                 If you plan to drive to the meeting, please self-park at the hotel and expense the parking charges to your cost center.
 
             </div>
@@ -109,7 +109,7 @@
             <div class="modal-body">
                 Follow our “business thoughtful” policy (jeans are acceptable) for daytime meetings. Consider bringing a light jacket or sweater as meeting rooms are often kept at cooler temps.
                 <br><br>
-                EVENING EVENTS<br>
+                <b>EVENING EVENTS</b><br>
                 Tuesday: Welcome Reception at the Hilton Anaheim (indoor event)<br>
                 Attire: Casual
                 <br><br>
@@ -119,9 +119,9 @@
                 Thursday: Awards Dinner, Ceremony and Celebration at the Hilton Anaheim (indoor event)<br>
                 Attire: Semi-formal—suit and tie for men, and equivalent for ladies
                 <br><br>
-                February weather in California averages between 60- and 75-degrees Fahrenheit. Expect cooler temps in the evening. For up-to-the-minute weather detail, click here.
+                February weather in California averages between 60- and 75-degrees Fahrenheit. Expect cooler temps in the evening. For up-to-the-minute weather detail, <a href="https://www.accuweather.com/en/us/anaheim/92805/weather-forecast/327150">click here</a>.
                 <br><br>
-                If you have questions about any planned Kickoff events, please contact kickoff@agilitihealth.com.
+                If you have questions about any planned Kickoff events, please contact <a href="https://kickoff@agilitihealth.com">kickoff@agilitihealth.com</a>.
 
             </div>
             <div class="modal-footer">
