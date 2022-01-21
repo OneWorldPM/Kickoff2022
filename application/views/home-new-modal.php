abab8841916f62bfbfa1d3928fdb1b4de9150cf7
Rexter Dayuta <rexterdayuta@gmail.com>
2022-01-22 02:36:46 +0800

    Homepage updates

--- application/views/home-new-modal.php
+++ application/views/home-new-modal.php
@@ -1,21 +1,21 @@
 <!-- Modal -->
 <div class="modal fade" id="agendaModal" tabindex="-1" aria-labelledby="agendaModal" aria-hidden="true">
     <div class="modal-dialog modal-lg">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="agendaModal">AGENDA</h5>
+        <div class="modal-content bg-brick">
+            <div class="modal-header bg-blur">
+                <h5 class="modal-title text-neon" id="agendaModal" >AGENDA</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
-            <div class="modal-body">
-                <b>REGISTRATION </b><br>
-                Registration will be open from 9:00 AM to 5:00 PM on Tuesday, February 22. Upon arrival at the Hilton, check-in at the front desk then head to the 2022 Commercial Kickoff registration desk on the second floor of the hotel to pick up your name badge. The Welcome Reception begins at 6:00 PM
+            <div class="modal-body bg-blur">
+                <p><b>REGISTRATION </b></p>
+                Registration will be open from 9 a.m. to 5 p.m. on Tuesday, February 22. Upon arrival at the Hilton, check-in at the front desk then head to the 2022 Commercial Kickoff registration desk on the second floor of the hotel to pick up your name badge. The Welcome Reception begins at 6 p.m.
                 <br><br>
+                <strong> AGENDA </strong><br>
                 Though a complete day-by-day agenda will be provided at the meeting registration desk, here’s an overview of the week (subject to change):
                 <br><br>
                 <b style="color: #fd7e14">Tuesday, February 22</b><br>
-                9:00 AM – 12:00 PM 	Leadership Registration<br>
                 12:00 PM – 5:00 PM  Leadership Lunch/Meeting<br>
                 2:00 PM – 5:00 PM	General Attendee Registration<br>
                 6:00 PM – 8:30 PM	Welcome Reception and Product/Solution Showcase<br><br>
@@ -42,7 +42,7 @@
                 All morning	Departures<br>
 
             </div>
-            <div class="modal-footer">
+            <div class="modal-footer bg-blur">
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
             </div>
         </div>
@@ -52,20 +52,20 @@
 <!-- Modal -->
 <div class="modal fade" id="travelModal" tabindex="-1" aria-labelledby="travelModalLavel" aria-hidden="true">
     <div class="modal-dialog modal-lg">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="travelModalLabel">TRAVEL & HOTEL</h5>
+        <div class="modal-content bg-brick">
+            <div class="modal-header bg-blur">
+                <h5 class="modal-title text-neon" id="travelModalLabel">TRAVEL & HOTEL</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
-            <div class="modal-body">
+            <div class="modal-body bg-blur">
                 This year’s hotel accommodations are based at the <a target="_blank" href="https://www3.hilton.com/en/hotels/california/hilton-anaheim-SNAAHHH/index.html?SEO_id=GMB-HI-SNAAHHH&y_source=1_MTIyMDk1OS03MTUtbG9jYXRpb24uZ29vZ2xlX3dlYnNpdGVfb3ZlcnJpZGU%3D">Hilton Anaheim</a>. Training sessions and several social events will take place inside the hotel.
                 <br><br>
                 Hotel reservations will be centrally managed by Todd Gabello, and a room will be held in your name once you complete your registration. Your confirmation number will be emailed to you no later than the week of February 14.
                 <br><br>
                 <b>ARRIVALS</b><br>
-                All attendees will arrive on Tuesday, February 22. Please refer to the January 17 email from <a href="mailto:kickoff@agilitihealth.com">kickoff@agilitihealth.com</a> for details on what time you need to be at the hotel by on Tuesday.
+                All attendees will arrive on Tuesday, February 22. Please refer to the January 17 email from <a class="text-neon-blue" href="mailto:kickoff@agilitihealth.com">kickoff@agilitihealth.com</a> for details on what time you need to be at the hotel by on Tuesday.
                 <br><br>
                 <b>DEPARTURES</b><br>
                 All attendees will depart on Friday, February 25. You may book your departure flight as early as you would like that morning from John Wayne Airport (SNA).
@@ -75,7 +75,7 @@
                 <br><br>
                 All attendees must book flights using the link that was embedded in the registration site. This will ensure that reservations are booked under the Agiliti account and will appear on our arrival/departure reports. If you book outside the registration site, your flight cost will not be covered by the 2022 Commercial Kickoff budget, and you will need to expense the flight through your individual cost center.
                 <br><br>
-                If you have any questions regarding your travel schedule, please contact <a href="mailto:Todd.Gabello@agilitihealth.com">Todd Gabello</a> .
+                If you have any questions regarding your travel schedule, please contact <a class="text-neon-blue" href="mailto:Todd.Gabello@agilitihealth.com">Todd Gabello</a> .
                 <br><br>
                 <b>GETTING AROUND TOWN</b><br>
                 Transportation to the Hilton Anaheim will be available upon arrival at John Wayne Airport (SNA). Greeters stationed in the baggage claim area with Agiliti signs will direct you to the nearest shuttle.
@@ -88,7 +88,7 @@
                 If you plan to drive to the meeting, please self-park at the hotel and expense the parking charges to your cost center.
 
             </div>
-            <div class="modal-footer">
+            <div class="modal-footer bg-blur">
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
             </div>
         </div>
@@ -98,32 +98,31 @@
 <!-- Modal -->
 <div class="modal fade" id="activityModal" tabindex="-1" aria-labelledby="activityModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="activityModalLabel">ACTIVITIES AND ATTIRE</h5>
+        <div class="modal-content bg-brick">
+            <div class="modal-header bg-blur">
+                <h5 class="modal-title text-neon" id="activityModalLabel">ACTIVITIES AND ATTIRE</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
-            <div class="modal-body">
+            <div class="modal-body bg-blur">
                 Follow our “business thoughtful” policy (jeans are acceptable) for daytime meetings. Consider bringing a light jacket or sweater as meeting rooms are often kept at cooler temps.
                 <br><br>
                 <b>EVENING EVENTS</b><br>
                 Tuesday: Welcome Reception at the Hilton Anaheim (indoor event)<br>
                 Attire: Casual
                 <br><br>
-                Wednesday: Offsite dinner (indoor/outdoor event)<br>
-                Attire: Casual and consider bringing a light jacket or sweater
+                Wednesday: Offsite dinner (indoor)<br>
                 <br><br>
                 Thursday: Awards Dinner, Ceremony and Celebration at the Hilton Anaheim (indoor event)<br>
                 Attire: Semi-formal; suit and tie for men, and equivalent for ladies
                 <br><br>
-                February weather in California averages between 60&deg and 75&deg Fahrenheit. Expect cooler temps in the evening. For up-to-the-minute weather detail, <a href="https://www.accuweather.com/en/us/anaheim/92805/weather-forecast/327150" target="_blank">click here</a>.
+                February weather in California averages between 60&deg and 75&deg Fahrenheit. Expect cooler temps in the evening. For up-to-the-minute weather detail, <a class="text-neon-blue" href="https://www.accuweather.com/en/us/anaheim/92805/weather-forecast/327150" target="_blank">click here</a>.
                 <br><br>
-                If you have questions about any planned Kickoff events, please contact <a href="mailto:kickoff@agilitihealth.com">kickoff@agilitihealth.com</a>.
+                If you have questions about any planned Kickoff events, please contact <a class="text-neon-blue" href="mailto:kickoff@agilitihealth.com">kickoff@agilitihealth.com</a>.
 
             </div>
-            <div class="modal-footer">
+            <div class="modal-footer bg-blur">
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
             </div>
         </div>
